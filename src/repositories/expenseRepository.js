2969061da168d1bfef09c9a82f0825ee6c603a29
welingtonlarsen <welingtonlarsen@pop-os.localdomain>
2024-01-10 00:12:13 -0300

    feat: improve get all expenses

--- src/repositories/expenseRepository.js
+++ src/repositories/expenseRepository.js
@@ -1,31 +1,43 @@
-const { PrismaClient } = require('@prisma/client');
-const { startOfMonth, endOfMonth, parse } = require('date-fns');
+const { PrismaClient } = require("@prisma/client");
+const { startOfMonth, endOfMonth, parse } = require("date-fns");
 
 const prisma = new PrismaClient();
 
 exports.createExpense = async (data) => {
   const isoDate = new Date(data.date);
-  return await prisma.expense.create({data: {
+  return await prisma.expense.create({
+    data: {
       ...data,
       date: isoDate,
-    }
+    },
   });
 };
 
 exports.getAllExpenses = async () => {
-  return prisma.expense.findMany()
-}
-
-exports.getExpenses = async (month, year) => {
-  const startDate = startOfMonth(parse(`${month} ${year}`, 'MMMM yyyy', new Date()));
-  const endDate = endOfMonth(startDate);
-
-  return await prisma.expense.findMany({
-    where: {
-      AND: [
-        { date: { gte: startDate } },
-        { date: { lt: endDate } }
-      ]
+  return prisma.expense.findMany({
+    include: {
+      category: true, // Inclui os dados da categoria
+      paymentMethod: true, // Inclui os dados do método de pagamento
+    },
+    orderBy: {
+      date: "desc",
     },
   });
-}
+};
+
+exports.getExpenses = async (month, year) => {
+  const startDate = startOfMonth(
+    parse(`${month} ${year}`, "MMMM yyyy", new Date())
+  );
+  const endDate = endOfMonth(startDate);
+
+  const x = await prisma.expense.findMany({
+    where: {
+      AND: [{ date: { gte: startDate } }, { date: { lt: endDate } }],
+    },
+    include: {
+      category: true, // Inclui os dados da categoria
+      paymentMethod: true, // Inclui os dados do método de pagamento
+    },
+  });
+};
--- src/services/expenseService.js
+++ src/services/expenseService.js
@@ -1,8 +1,8 @@
-const expenseRepository = require('../repositories/expenseRepository');
+const expenseRepository = require("../repositories/expenseRepository");
 
 exports.createExpense = async (data) => {
   let installments = 1;
-  let currentInstallment = 1
+  let currentInstallment = 1;
 
   // Supondo que 'CREDIT_CARD' tenha o ID 1 na tabela PaymentMethod
   if (data.paymentMethodId === 1 && data.installments) {
@@ -13,17 +13,21 @@ exports.createExpense = async (data) => {
     currentInstallment = data.currentInstallment;
   }
 
-  return await expenseRepository.createExpense({...data, installments, currentInstallment});
+  return await expenseRepository.createExpense({
+    ...data,
+    installments,
+    currentInstallment,
+  });
 };
 
 exports.getAllExpenses = async (month, year) => {
-  if(month && year) {
-    return expenseRepository.getExpenses(month, year)
+  if (month && year) {
+    return expenseRepository.getExpenses(month, year);
   } else {
-    return expenseRepository.getAllExpenses()
+    return expenseRepository.getAllExpenses();
   }
-}
+};
 
 exports.sumExpenses = (expenses) => {
-  return expenses.reduce((sum, current) => sum + current.amount, 0)
-}
+  return expenses.reduce((sum, current) => sum + current.amount, 0);
+};
